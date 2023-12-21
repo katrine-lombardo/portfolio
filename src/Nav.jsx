d56cce9cd4377030f5e733aa5382726db4b32380
Katrine Lombardo <katrine.lombardo@gmail.com>
2023-12-21 13:52:12 -0500

    [Home]: Adjusted component viewport for homepage

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,67 +4,69 @@ import { NavLink } from "react-router-dom";
 function Nav() {
   return (
     <>
-      <ul className='flex'>
-        <li className='mr-6'>
-          <NavLink to='/' end>
-            <img
-              src='badingo_clean.png'
-              className='w-12 rounded-lg ...'
-              alt='badingo_clean'
-            />
-          </NavLink>
-        </li>
-        <li className='flex-grow'></li>
-        <li className='mr-6'>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "text-blue-900 hover:text-blue-800"
-                : "text-blue-500 hover:text-blue-800"
-            }
-            to='/'
-            end
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className='mr-6'>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "text-blue-900 hover:text-blue-800"
-                : "text-blue-500 hover:text-blue-800"
-            }
-            to='/projects'
-          >
-            Projects
-          </NavLink>
-        </li>
-        <li className='mr-6'>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "text-blue-900 hover:text-blue-800"
-                : "text-blue-500 hover:text-blue-800"
-            }
-            to='/skills'
-          >
-            Skills
-          </NavLink>
-        </li>
-        <li className='mr-6'>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "text-blue-900 hover:text-blue-800"
-                : "text-blue-500 hover:text-blue-800"
-            }
-            to='/about'
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
+      <div className='mb-8'>
+        <ul className='flex'>
+          <li className='mr-6'>
+            <NavLink to='/' end>
+              <img
+                src='badingo_clean.png'
+                className='w-12 rounded-lg'
+                alt='badingo_clean'
+              />
+            </NavLink>
+          </li>
+          <li className='flex-grow'></li>
+          <li className='mr-6'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-blue-900 hover:text-blue-800"
+                  : "text-blue-500 hover:text-blue-800"
+              }
+              to='/'
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className='mr-6'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-blue-900 hover:text-blue-800"
+                  : "text-blue-500 hover:text-blue-800"
+              }
+              to='/projects'
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li className='mr-6'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-blue-900 hover:text-blue-800"
+                  : "text-blue-500 hover:text-blue-800"
+              }
+              to='/skills'
+            >
+              Skills
+            </NavLink>
+          </li>
+          <li className='mr-6'>
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-blue-900 hover:text-blue-800"
+                  : "text-blue-500 hover:text-blue-800"
+              }
+              to='/about'
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,18 +1,27 @@
 function HomePage() {
   return (
-    <>
-      <p>This is the homepage</p>
-      <div>
-        <img
-          src='domino.jpeg'
-          className='rounded-lg shadow-2xl ...'
-          alt='Domino'
-        />
+    <div className='h-[calc(100vh-150px)] flex items-center justify-center'>
+      <div className='grid grid-cols-2 gap-10'>
+        <div class='px-1 w-full'>
+          <img
+            src='domino.jpeg'
+            className='w-full aspect-square object-cover rounded-full border-2'
+            alt='Domino'
+          />
+        </div>
+        <div className='flex flex-col justify-center'>
+          <div>
+            <h1>Welcome to Badingo.net</h1>
+          </div>
+          <div>
+            <p>This is the homepage</p>
+          </div>
+          <div>
+            <p className='font-serif'>Am I changing?</p>
+          </div>
+        </div>
       </div>
-      <h1>Hello Domino</h1>
-
-      <p className='font-serif ...'>Am I changing?</p>
-    </>
+    </div>
   );
 }
 export default HomePage;
