ee48aaa6a22e2ffc4ecb73b4928928ff8785903b
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-08 15:27:48 -0500

    [Skills]: Added my tools content, icons, and styling

--- src/components/ListTools.jsx
+++ src/components/ListTools.jsx
@@ -1,4 +1,47 @@
+import "../assets/Logos.css";
+
 const ListTools = () => {
-  return <p>ListTools component goes here</p>;
+  return (
+    <div>
+      <div class='flex overflow-x-auto space-x-7'>
+        <section class='flex-shrink-1'>
+          <div class='icons8-docker'></div>
+          <span>Docker</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-tailwind'></div>
+          <span>Tailwind</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-bootstrap'></div>
+          <span>Bootstrap</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-github'></div>
+          <span>Github</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-gitlab'></div>
+          <span>Gitlab</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-rest'></div>
+          <span>RESTful API</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-cursor'></div>
+          <span>Cursor IDE</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-google-drive'></div>
+          <span>Google Drive</span>
+        </section>
+        <section class='flex-shrink-1'>
+          <div class='icons8-excel'></div>
+          <span>Excel</span>
+        </section>
+      </div>
+    </div>
+  );
 };
 export default ListTools;
--- src/views/Skills.jsx
+++ src/views/Skills.jsx
@@ -6,21 +6,17 @@ const Skills = () => {
   return (
     <>
       <div className='container mx-auto'>
-        <div className='container'>
-          <h1>My Skills</h1>
+        <div className='container pb-6'>
+          <h1>My Technical Skills</h1>
           <ListSkills />
         </div>
-        <div className='container'>
+        <div className='container py-6'>
           <h1>My Tools</h1>
-          <div className='overflow-x-scroll'>
-            <ListTools />
-          </div>
+          <ListTools />
         </div>
-        <div className='container'>
+        <div className='container py-6'>
           <h1>My Certifications</h1>
-          <div className='overflow-x-scroll'>
-            <ListCertifications />
-          </div>
+          <ListCertifications />
         </div>
       </div>
     </>
