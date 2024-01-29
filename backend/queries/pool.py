dfc9c8cf877727b487eacc782db010eda13b0cfa
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-29 17:51:29 -0500

    [phptutorial]: Not able to log in to phpmyadmin

--- backend/queries/pool.py
+++ backend/queries/pool.py
@@ -2,11 +2,11 @@ import os
 import mysql.connector
 
 db_config = {
-    "host": "localhost",
-    "user": "gr33nguy",
-    "password": "******",
-    "database": "portfolio",
-    "port": 3306,
+    "host": os.environ.get("DB_HOST"),
+    "user": os.environ.get("DB_USERNAME"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_DATABASE"),
+    "port": int(os.environ.get("DB_PORT")),
 }
 
 connection = mysql.connector.connect(**db_config)
