e8b1426525ab38e476e427ed4376b971f1c187ba
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-29 16:04:03 -0500

    [php]: Tutorial was overly complex, cutting losses and trying again with simplified doc

--- api/Model/CommentsModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once PROJECT_ROOT_PATH . "/Model/Database.php";
-class CommentsModel extends Database
-{
-    public function getComments($limit)
-    {
-        return $this->select("SELECT * FROM comments ORDER BY id ASC LIMIT ?", ["i", $limit]);
-    }
-}
