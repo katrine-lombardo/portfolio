4e6b94383bc45ec42eb32ff08d9efa55014a1266
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-13 16:54:22 -0500

    [Home]: Corrected breakpoints for homepage

--- react/src/components/AboutMe.jsx
+++ react/src/components/AboutMe.jsx
@@ -1,38 +1,36 @@
 const AboutMe = () => {
   return (
-    <div>
-      <h1 className='mb-4'>About Me</h1>
-      <div className='flex flex-wrap gap-4 justify-center lg:flex-no-wrap'>
-        <div className='basis-3/5'>
-          <p className='my-2'>Hi, I'm Katrine!</p>
-          <p>
-            I'm a recent Hack Reactor coding bootcamp graduate and seasoned
-            workplace leader with a passion for problem-solving.
-          </p>
-          <p className='my-2'>
-            I aim to use cutting-edge technology for innovative problem-solving
-            and actively promote diversity in the tech industry. With a strong
-            work ethic and a multicultural background (born in Scotland,
-            Norwegian as my first language), I see myself as a bridge-builder
-            connecting ideas, people, and solutions.
-          </p>
-          <p className='my-2'>
-            In my 10-year HR journey at tech startups, I've demonstrated
-            determination, loyalty, and humor, refining skills in data and
-            systems. I've been told I bring a "special sauce" of resilience,
-            continuous growth, humour, and authenticity.
-          </p>
-          <p className='my-2'>
-            Excited to connect, collaborate, and contribute to the ever-evolving
-            landscape of technology! 🚀
-          </p>
-        </div>
-        <div className='flex-none md:shrink-0'>
+    <div className='my-32 mx-1'>
+      <div className='flex flex-col-reverse md:flex-row gap-6 justify-center items-center'>
+        <div className='flex-none md:order-1 md:shrink-0'>
           <img
-            src='katrine.png'
-            className='w-fit aspect-square object-cover rounded-full border-2 object-none object-top'
-            alt='Katrine'
+            src='katrine_and_domino.jpeg'
+            className='w-96 aspect-square object-cover rounded-full border-2'
+            alt='Domino'
           />
+        </div>
+        <div className='md:basis-2/5 mx-8 md:order-2'>
+          <div>
+            <h1 className='font-medium text-3xl my-4 md:text-left'>
+              Hello, World
+            </h1>
+          </div>
+          <div>
+            <p className='md:text-left my-3'>
+              Hello, I'm Katrine! It's nice to meet you. Welcome to badingo.net,
+              my portfolio page built in Vite + React, deployed on Plesk, and
+              domain hosted on GoDaddy.
+            </p>
+            <p className='md:text-left my-3'>
+              I'm a recent Hack Reactor coding bootcamp graduate and seasoned
+              workplace leader with 10+ years experience supporting rapidly
+              scaling tech startups, and a passion for problem-solving.
+            </p>
+            <p className='md:text-left my-3'>
+              Make yourself at home, view my projects and skills, and chat with
+              me about how we can work together to build something cool.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -2,40 +2,10 @@ import AboutMe from "../components/AboutMe";
 
 const HomePage = () => {
   return (
-    <div className='my-32 mx-1'>
-      <div className='flex flex-wrap gap-4 justify-center items-center lg:flex-no-wrap'>
-        <div className='flex-none md:shrink-0'>
-          <img
-            src='katrine_and_domino.jpeg'
-            className='w-96 aspect-square object-cover rounded-full border-2'
-            alt='Domino'
-          />
-        </div>
-        <div className='basis-2/5'>
-          <div>
-            <h1 className='font-medium text-3xl my-4 md:text-left'>
-              Hello, World
-            </h1>
-          </div>
-          <div>
-            <p className='md:text-left my-3'>
-              Hello, I'm Katrine! It's nice to meet you. Welcome to badingo.net,
-              my portfolio page built in Vite + React, deployed on Plesk, and
-              domain hosted on GoDaddy.
-            </p>
-            <p className='md:text-left my-3'>
-              I'm a recent Hack Reactor coding bootcamp graduate and seasoned
-              workplace leader with 10+ years experience supporting rapidly
-              scaling tech startups, and a passion for problem-solving.
-            </p>
-            <p className='md:text-left my-3'>
-              Make yourself at home, view my projects and skills, and chat with
-              me about how we can work together to build something cool.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div>
+      <AboutMe />
     </div>
   );
 };
+
 export default HomePage;
