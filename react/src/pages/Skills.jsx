d22c17774d379ab6ccd8f538dc763aff0b9b6990
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-13 18:12:35 -0500

    [Skills]: Responsive overscroll behaviour working as expected now

--- react/src/pages/Skills.jsx
+++ react/src/pages/Skills.jsx
@@ -4,22 +4,22 @@ import ListCertifications from "../components/ListCertifications";
 
 const Skills = () => {
   return (
-    <>
+    <div className='mx-8'>
       <div className='flex flex-col mx-auto my-9 mx-7'>
-        <div className='my-3'>
-          <h1 className='text-left mb-1'>My Technical Skills</h1>
+        <div className='my-5'>
+          <h1 className='text-center mb-1'>My Technical Skills</h1>
           <ListSkills />
         </div>
-        <div className='my-3'>
-          <h1 className='text-left mb-1'>My Tools</h1>
+        <div className='my-5'>
+          <h1 className='text-center mb-1'>My Tools</h1>
           <ListTools />
         </div>
-        <div className='my-3'>
-          <h1 className='text-left mb-1'>My Certifications</h1>
+        <div className='my-5'>
+          <h1 className='text-center mb-1'>My Certifications</h1>
           <ListCertifications />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Skills;
