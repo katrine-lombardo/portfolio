6fea9498819958aa06bf72f9600c5f97efa7dc93
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-01 13:32:58 -0500

    [contact-form]: Added returned components for error and success messages, next step is making them dynamic based on user input

--- react/src/pages/Contact.jsx
+++ react/src/pages/Contact.jsx
@@ -1,8 +1,16 @@
 import FormContact from "../components/FormContact";
+import SubmitSuccess from "../components/SubmitSuccess";
+import SubmitError from "../components/SubmitError";
 
 const Contact = () => {
   return (
     <div className='columns-1 p-8'>
+      <div className='mx-auto'>
+        <SubmitError />
+      </div>
+      <div className='mx-auto'>
+        <SubmitSuccess />
+      </div>
       <div className='flex justify-center'>
         <FormContact />
       </div>
