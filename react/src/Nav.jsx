9cb8f5cb80ea13dff88c3af9bbdb701419fdf630
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-04 16:58:26 -0500

    [navbar]: Added x button to close menu

--- react/src/Nav.jsx
+++ react/src/Nav.jsx
@@ -81,14 +81,25 @@ const Nav = () => {
         onClick={toggleNav}
         className='block md:hidden items-center px-3 py-2 border rounded text-rose-300 border-rose-300 bg-rose-100 hover:text-rose-600 hover:border-rose-600 hover:bg-rose-200'
       >
-        <svg
-          className={`${openNav ? "hidden" : "block fill-current h-3 w-3"}`}
-          viewBox='0 0 20 20'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <title>Menu</title>
-          <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-        </svg>
+        {openNav ? (
+          <svg
+            className='fill-current h-3 w-3'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <title>Close</title>
+            <path d='M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z' />
+          </svg>
+        ) : (
+          <svg
+            className='fill-current h-3 w-3'
+            viewBox='0 0 20 20'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <title>Menu</title>
+            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+          </svg>
+        )}
       </button>
     </div>
   );
